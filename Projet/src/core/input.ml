3b2962ea9fd79a7309aaae8169f277c812c47c3b
Anthang <anthonygiang012904@gmail.com>
2025-02-23 12:30:26 +0100

    debut de ne pas differencier touche long et court . Pb: ne fonctionne que si on appuye sur une touche unique et c'est ne differencie que partiellement c'est a dire que il enleve la touche apres un certain temps

--- Projet/src/core/input.ml
+++ Projet/src/core/input.ml
@@ -1,4 +1,6 @@
 let key_table = Hashtbl.create 16
+let table_key_pressed : (string, unit) Hashtbl.t = Hashtbl.create 16
+
 let has_key s = Hashtbl.mem key_table s
 let set_key s= Hashtbl.replace key_table s ()
 let unset_key s = Hashtbl.remove key_table s
@@ -8,15 +10,17 @@ let register key action = Hashtbl.replace action_table key action
 
 
 let handle_input () =
-
-
   let () =
     match Gfx.poll_event () with
-    | KeyDown s ->set_key s;
-    | KeyUp s -> unset_key s
+    | KeyDown s ->
+      if not (Hashtbl.mem table_key_pressed s) then begin
+        set_key s;
+        Hashtbl.replace table_key_pressed s ()
+      end else unset_key s
+    | KeyUp s -> unset_key s ;Hashtbl.remove table_key_pressed s 
     | Quit -> exit 0
     | _ -> ()
-  in
+  in 
 
 
   Hashtbl.iter (fun key action ->
