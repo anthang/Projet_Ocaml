5c1ffe43288ce49e7423ac54ba01b383003ec2ec
Anthang <anthonygiang012904@gmail.com>
2025-02-27 17:31:15 +0100

    modification de la table de hachage (string, unit) en (string,float) pour le temps

--- Projet/src/core/input.ml
+++ Projet/src/core/input.ml
@@ -1,5 +1,5 @@
 let key_table = Hashtbl.create 16
-let table_key_pressed : (string, unit) Hashtbl.t = Hashtbl.create 16
+let table_key_pressed : (string, float) Hashtbl.t = Hashtbl.create 16 
 
 let has_key s = Hashtbl.mem key_table s
 let set_key s= Hashtbl.replace key_table s ()
@@ -8,24 +8,53 @@ let unset_key s = Hashtbl.remove key_table s
 let action_table = Hashtbl.create 16
 let register key action = Hashtbl.replace action_table key action
 
+let get_key_duration key =
+  try Unix.gettimeofday () -. Hashtbl.find table_key_pressed key
+  with Not_found -> 0.0
 
-let handle_input () =
+let rec handle_input () =
+  let current_time = Unix.gettimeofday () in
   let () =
     match Gfx.poll_event () with
     | KeyDown s ->
-      if not (Hashtbl.mem table_key_pressed s) then begin
-        set_key s;
-        Hashtbl.replace table_key_pressed s ()
-      end else unset_key s
-    | KeyUp s -> unset_key s ;Hashtbl.remove table_key_pressed s 
+      set_key s;
+      if(s = "i") then 
+      Hashtbl.replace table_key_pressed s current_time
+    else ()
+    | KeyUp s -> unset_key s ;
+    Hashtbl.remove table_key_pressed s 
     | Quit -> exit 0
     | _ -> ()
   in 
 
-
+(*
   Hashtbl.iter (fun key action ->
-      if has_key key then action () else Player.(move_player (player1()) Cst.gravitie) ) action_table
+    if has_key key then 
+      if key = "i" then 
+        if (Unix.gettimeofday () -. Hashtbl.find table_key_pressed "i") > 0.5 then
+          Player.(move_player (player1()) Cst.gravitie)
+        else
+          action ()
+      else
+        action ()
+    else 
+      Player.(move_player (player1()) Cst.gravitie)
+  ) action_table
+*)
 
+if(Hashtbl.mem table_key_pressed "i") then
+  Hashtbl.iter (fun key action ->
+    if has_key key then 
+      if key = "i" then 
+        if (Unix.gettimeofday () -. Hashtbl.find table_key_pressed "i") > 0.5 then
+          Player.(move_player (player1()) Cst.gravitie)
+        else
+          action ()
+      else
+        action ()
+    else 
+      Player.(move_player (player1()) Cst.gravitie)
+  ) action_table
 
 let () =
   register "i" (fun () -> Player.(move_player (player1()) Cst.paddle_v_up));
